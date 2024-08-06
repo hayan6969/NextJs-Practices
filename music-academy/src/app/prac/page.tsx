dc65aaf0dbe95ba2a0baddbba6d375d4a772dfe0
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-06 11:44:56 +0500

    ..

--- music-academy/src/app/prac/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function prac() {
-  return (
-    <div className="text-3xl text-center mt-8 font-bold">Prac</div>
-  )
-}
-
--- music-academy/src/app/prac/try/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Try() {
-  return (
-    <div className="text-3xl text-center mt-8 font-bold">Try</div>
-  )
-}
