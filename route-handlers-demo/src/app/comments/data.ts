056f30f56437d6036650f391ac16e71f219c17d1
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-12 21:34:19 +0500

    ...

---
+++ route-handlers-demo/src/app/comments/data.ts
@@ -0,0 +1,24 @@
+//array of objects having 5 comment objects with id and text
+
+export const comments = [
+  {
+    id: 1,
+    text: 'Comment 1',
+  },
+  {
+    id: 2,
+    text: 'Comment 2',
+  },
+  {
+    id: 3,
+    text: 'Comment 3',
+  },
+  {
+    id: 4,
+    text: 'Comment 4',
+  },
+  {
+    id: 5,
+    text: 'Comment 5',
+  },
+];
