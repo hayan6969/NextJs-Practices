8766935e2c31339291d8bade069bec7cbab22146
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-13 15:46:02 +0500

    cookies

--- route-handlers-demo/src/app/profile/api/route.tsx
+++ route-handlers-demo/src/app/profile/api/route.tsx
@@ -1,9 +1,20 @@
 import { type NextRequest } from "next/server"
-import  {headers} from "next/headers"
+import  {headers,cookies} from "next/headers"
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers)
     const headerList=headers();
+
+    cookies().set("results per page","20")
+    
+    console.log(cookies().get("results per page"))
+    const theme=request.cookies.get("theme")
+    console.log(theme)
     console.log(requestHeaders.get("Authorization"))
     console.log(headerList.get("Authorization"))
-    return new Response("profile api data")
+    return new Response("<h1>profile api data</h1>",{
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        }
+    })
 }
