8b54b4be76fbe059d7cc0dbd68c72880ee93476f
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-13 22:32:44 +0500

    ...

---
+++ rendering/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { useState } from 'react'
+function AboutPage() {
+    const [name, setName] = useState('')
+  return (
+    <div>AboutPage</div>
+  )
+}
+
+export default AboutPage
