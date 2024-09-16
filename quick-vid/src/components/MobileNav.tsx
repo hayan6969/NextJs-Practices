cc0443b884eee6224ee573ccfe27283c6a6b62e1
Hayan Khan <hayankhanmehsood@gmail.com>
2024-09-16 13:58:43 +0500

    ...

--- quick-vid/src/components/MobileNav.tsx
+++ quick-vid/src/components/MobileNav.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from "next/image"
+  
 
 function MobileNav() {
   return (
     <section className='w-full max-w-[264px]'>
-        
+
+<Sheet>
+  <SheetTrigger>
+    <Image
+    src="/icons/hamburger.svg"
+    alt="menu"
+    width={36}
+    height={36}
+    className="cursor-pointer sm:hidden"
+    />
+  </SheetTrigger>
+  <SheetContent side={'left'}>
+    <SheetHeader>
+      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
 
     </section>
   )
