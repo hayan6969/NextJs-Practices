d413a630b4b5b3d64fb1cf6df414c8636bfa5011
Hayan Khan <hayankhanmehsood@gmail.com>
2024-09-17 16:11:25 +0500

    ..

--- quick-vid/src/app/(root)/meeting/[id]/page.tsx
+++ quick-vid/src/app/(root)/meeting/[id]/page.tsx
@@ -1,10 +1,25 @@
+'use client'
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
 import React from 'react'
+import { isSet } from 'util/types'
 
 const Meeting = ({params}:{params:{id:string}}) => {
+  
+  const {user, isLoaded} = useUser()
+  const [isSetupComplete, setIsSetupComplete] = React.useState(false)   
   return (
-    <div>
-      Meeting room no. {params.id}
-    </div>
+   <main className='h-screen w-full '>
+<StreamCall>
+  <StreamTheme>
+{
+  !isSetupComplete ? ('Meeting Setup'):('Meeting Room')
+}
+  </StreamTheme>
+</StreamCall>
+
+    
+   </main>
   )
 }
 
